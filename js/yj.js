e42764d8744502e55fbc205c5c2f7d401eaa2094
wuhuaviator <3476059264@qq.com>
2020-11-23 10:49:03 +0800

    Site updated: 2020-11-23 10:48:54

---
+++ js/yj.js
@@ -0,0 +1,3 @@
+$(document).ready(function(e) {
+    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Penguin');
+});
